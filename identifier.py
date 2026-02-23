8ffac10aa322e7a580f82d3cf41fb20b8f4c0c57
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-02-23 21:53:07 +0530

    identifier

---
+++ identifier.py
@@ -0,0 +1,15 @@
+var = 10
+# Var = 20
+# VaR = 30
+print(var)
+# print(Var)
+# print(VaR)
+
+class A:
+    def m1(self):
+        print("Faizan")
+        print("Faiz")
+        print("Faizy")
+
+r=A()
+r.m1()
