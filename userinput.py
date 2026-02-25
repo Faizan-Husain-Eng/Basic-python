92db5169475226b54558faf7a3af426fe4440001
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-02-25 18:36:28 +0530

    userinput

---
+++ userinput.py
@@ -0,0 +1,8 @@
+a = input("my name is faizan:")
+print("My name is faizy", a)
+
+x = input("Enter first number: ")
+y = input("enter second number: ") 
+# print(x + y)faizan
+
+print(int(x) + int(y))
